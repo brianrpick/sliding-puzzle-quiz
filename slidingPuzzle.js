f43c61209ba6650d443df9c10c26d43b2da818aa
Brian Pick <brianr.pick@gmail.com>
2017-05-03 16:56:18 -0700

    Added coordinates to puzzle instead of removing styling to make sliding work

--- slidingPuzzle.js
+++ slidingPuzzle.js
@@ -4,12 +4,59 @@
 //  check if tile is next to opening
 //    if open is next to it, move tile
     //making sure clicked target is the right one
+document.addEventListener("DOMContentLoaded", initialize);
 
-
-// function initialize() {
+function initialize() {
     var targetTile = [];
     var emptyAdjacent = true;
-
+    var startingSpot = [
+        tile1 = {
+            position: 1,
+            top: '0',
+            left: '0'
+        },
+        tile2 = {
+            position: 1,
+            top: '0',
+            left: '230'
+        },
+        tile3 = {
+            position: 1,
+            top: '0',
+            left: '460'
+        },
+        tile4 = {
+            position: 1,
+            top: '230',
+            left: '0'
+        },
+        tile5 = {
+            position: 1,
+            top: '230',
+            left: '230'
+        },
+        tile6 = {
+            position: 1,
+            top: '230',
+            left: '460'
+        },
+        tile7 = {
+            position: 1,
+            top: '460',
+            left: '0'
+        },
+        tile8 = {
+            position: 1,
+            top: '460',
+            left: '230'
+        },
+        empty = {
+            position: 1,
+            top: '460',
+            left: '460'
+        },
+    ];
+    tileListener();
     // Adding event listener for clicked tiles
     function tileListener() {
         var tiles = Array.from(document.querySelectorAll('.tile'));
@@ -17,14 +64,7 @@
         //grabbing clicked tile
         function clickedTile(e) {
             var targetTile = e.srcElement;
-            if (emptyAdjacent = true){
-                //adding id to make invisible
-                targetTile.setAttribute("id", "down")
-            };
         };
     };
+}
 
-
-
-// }
-window.onload = tileListener;
