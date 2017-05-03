21a03c899c0f3ea9d7e603bd5cc254cc966f4ff6
Brian Pick <brianr.pick@gmail.com>
2017-05-03 13:14:12 -0700

    Moved styling to separate file, started outline for js

--- slidingPuzzle.js
+++ slidingPuzzle.js
@@ -1,1 +1,5 @@
-slidingPuzzle.js
+//some type of object(hash?) holding values of tiles
+//need to know where empty space is
+//need to know which tile is clicked
+//  check if tile is next to opening
+//    if open is next to it, move tile
