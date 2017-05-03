58e3463d7297e59425358422f0dac51fe0224920
Brian Pick <brianr.pick@gmail.com>
2017-05-03 14:11:27 -0700

    Event listener for click working

--- slidingPuzzle.js
+++ slidingPuzzle.js
@@ -3,3 +3,15 @@
 //need to know which tile is clicked
 //  check if tile is next to opening
 //    if open is next to it, move tile
+
+
+// Adding event listner for clicked tiles
+function findTile() {
+    var tiles = Array.from(document.querySelectorAll('.tile'));
+    tiles.forEach(tile => tile.addEventListener('click', clickedTile));
+}
+//making sure clicked target is the right now
+function clickedTile(e) {
+    console.log(e.target || e.srcElement)
+}
+window.onload = findTile;
